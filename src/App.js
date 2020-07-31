1aaf7f4a4a5833743d6bacec672a0c508db56370
Antonio Hermosilla <hermosilla.everis@gmail.com>
2020-07-31 19:47:57 +0200

    Auth

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
+import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react';
 import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Hello Lawyer !!
-        </p>
+      <header>
+        <h1>Authentication</h1>
       </header>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
